69debf2761825449451eeeb489bd38d7091c660e
Gian Paul Bonares <rorymercury1777@gmail.com>
2025-03-08 16:19:42 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// * Dice roller program
+
+function rollDice(){
+    const numOfDice = document.getElementById('numDice').value;
+    const diceResult = document.getElementById('diceResult');
+    const diceImages = document.getElementById('diceImages');
+    const values = [];
+    const images = [];
+
+    for(let i = 0; i < numOfDice; i++){
+        const value = Math.floor(Math.random() * 6) + 1;
+        values.push(value);
+        images.push(`<img src="Dice/${value}.png">`);
+    }
+
+    diceResult.textContent = `dice: ${values.join(', ')}`;
+    diceImages.innerHTML = images.join(''); // Add this line to update the diceImages element
+}
